2d1206ca7833c9189e68fea00c35efcf5b33a07d
hein09 <sebastian_gsaenger@web.de>
2015-07-21 23:12:42 +0200

    functions for 8-bit arithmetic/logic

--- cpu.c
+++ cpu.c
@@ -3,43 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum opcodes{
-NOP,     LD_BC_w, LD_dBC_A,  INC_BC, INC_B,   DEC_B,   LD_B_b,   RLCA, LD_dw_SP, ADD_HL_BC, LD_A_dBC,  DEC_BC, INC_C, DEC_C, LD_C_b, RRCA,
-STOP,    LD_DE_w, LD_dDE_A,  INC_DE, INC_D,   DEC_D,   LD_D_b,   RLA,  JR_b,     ADD_HL_DE, LD_A_dDE,  DEC_DE, INC_E, DEC_E, LD_E_b, RRA,
-JR_NZ_b, LD_HL_w, LD_dHLp_A, INC_HL, INC_H,   DEC_H,   LD_H_b,   DAA,  JR_Z_b,   ADD_HL_HL, LD_A_dHLp, DEC_HL, INC_L, DEC_L, LD_L_b, CPL,
-JR_NC_b, LD_SP_w, LD_dHLm_A, INC_SP, INC_dHL, DEC_dHL, LD_dHL_b, SCF,  JR_C_b,   ADD_HL_SP, LD_A_dHLm, DEC_SP, INC_A, DEC_A, LD_A_b, CCF,
-LD_B_B,   LD_B_C,   LD_B_D,   LD_B_E,   LD_B_H,   LD_B_L,   LD_B_dHL, LD_B_A,   LD_C_B, LD_C_C, LD_C_D, LD_C_E, LD_C_H, LD_C_L, LD_C_dHL, LD_C_A,
-LD_D_B,   LD_D_C,   LD_D_D,   LD_D_E,   LD_D_H,   LD_D_L,   LD_D_dHL, LD_D_A,   LD_E_B, LD_E_C, LD_E_D, LD_E_E, LD_E_H, LD_E_L, LD_E_dHL, LD_E_A,
-LD_H_B,   LD_H_C,   LD_H_D,   LD_H_E,   LD_H_H,   LD_H_L,   LD_H_dHL, LD_H_A,   LD_L_B, LD_L_C, LD_L_D, LD_L_E, LD_L_H, LD_L_L, LD_L_dHL, LD_L_A,
-LD_dHL_B, LD_dHL_C, LD_dHL_D, LD_dHL_E, LD_dHL_H, LD_dHL_L, HALT,     LD_dHL_A, LD_A_B, LD_A_C, LD_A_D, LD_A_E, LD_A_H, LD_A_L, LD_A_dHL, LD_A_A,
-ADD_A_B, ADD_A_C, ADD_A_D, ADD_A_E, ADD_A_H, ADD_A_L, ADD_A_dHL, ADD_A_A, ADC_A_B, ADC_A_C, ADC_A_D, ADC_A_E, ADC_A_H, ADC_A_L, ADC_A_dHL, ADC_A_A,
-SUB_B,   SUB_C,   SUB_D,   SUB_E,   SUB_H,   SUB_L,   SUB_dHL,   SUB_A,   SBC_A_B, SBC_A_C, SBC_A_D, SBC_A_E, SBC_A_H, SBC_A_L, SBC_A_dHL, SBC_A_A,
-AND_B,   AND_C,   AND_D,   AND_E,   AND_H,   AND_L,   AND_dHL,   AND_A,   XOR_B,   XOR_C,   XOR_D,   XOR_E,   XOR_H,   XOR_L,   XOR_dHL,   XOR_A,
-OR_B,    OR_C,    OR_D,    OR_E,    OR_H,    OR_L,    OR_dHL,    OR_A,    CP_B,    CP_C,    CP_D,    CP_E,    CP_H,    CP_L,    CP_dHL,    CP_A, 
-RET_NZ,   POP_BC, JP_NZ_w, JP_w,  CALL_NZ_w, PUSH_BC, ADD_A_b, RST_00H, RET_Z,      RET,      JP_Z_w,  CB,    CALL_Z_w, CALL_w, ADC_A_b, RST_08H,
-RET_NC,   POP_DE, JP_NC_w, fail1, CALL_NC_w, PUSH_DE, SUB_b,   RST_10H, RET_C,      RETI,     JP_C_w,  fail2, CALL_C_w, fail3,  SBC_A_b, RST_18H,
-LDH_db_A, POP_HL, LD_dC_A, fail4, fail5,     PUSH_HL, AND_b,   RST_20H, ADD_SP_b,   JP_dHL,   LD_dw_A, fail6, fail7,    fail8,  XOR_b,   RST_28H,
-LDH_A_db, POP_AF, LD_A_dC, DI,    fail9,     PUSH_AF, OR_b,    RST_30H, LD_HL_SP_b, LD_SP_HL, LD_A_dw, EI,    failA,    failB,  CP_b,    RST_38H
-};
-
-enum opcodesCB{
-RLC_B, RLC_C, RLC_D, RLC_E, RLC_H, RLC_L, RLC_dHL, RLC_A, RRC_B, RRC_C, RRC_D, RRC_E, RRC_H, RRC_L, RRC_dHL, RRC_A,
-RL_B, RL_C, RL_D, RL_E, RL_H, RL_L, RL_dHL, RL_A, RR_B, RR_C, RR_D, RR_E, RR_H, RR_L, RR_dHL, RR_A,
-SLA_B, SLA_C, SLA_D, SLA_E, SLA_H, SLA_L, SLA_dHL, SLA_A, SRA_B, SRA_C, SRA_D, SRA_E, SRA_H, SRA_L, SRA_dHL, SRA_A,
-SWAP_B, SWAP_C, SWAP_D, SWAP_E, SWAP_H, SWAP_L, SWAP_dHL, SWAP_A, SRL_B, SRL_C, SRL_D, SRL_E, SRL_H, SRL_L, SRL_dHL, SRL_A,
-BIT_0_B, BIT_0_C, BIT_0_D, BIT_0_E, BIT_0_H, BIT_0_L, BIT_0_dHL, BIT_1_B, BIT_1_C, BIT_1_D, BIT_1_E, BIT_1_H, BIT_1_L, BIT_1_dHL, 
-BIT_2_B, BIT_2_C, BIT_2_D, BIT_2_E, BIT_2_H, BIT_2_L, BIT_2_dHL, BIT_3_B, BIT_3_C, BIT_3_D, BIT_3_E, BIT_3_H, BIT_3_L, BIT_3_dHL, 
-BIT_4_B, BIT_4_C, BIT_4_D, BIT_4_E, BIT_4_H, BIT_4_L, BIT_4_dHL, BIT_5_B, BIT_5_C, BIT_5_D, BIT_5_E, BIT_5_H, BIT_5_L, BIT_5_dHL, 
-BIT_6_B, BIT_6_C, BIT_6_D, BIT_6_E, BIT_6_H, BIT_6_L, BIT_6_dHL, BIT_7_B, BIT_7_C, BIT_7_D, BIT_7_E, BIT_7_H, BIT_7_L, BIT_7_dHL, 
-RES_0_B, RES_0_C, RES_0_D, RES_0_E, RES_0_H, RES_0_L, RES_0_dHL, RES_1_B, RES_1_C, RES_1_D, RES_1_E, RES_1_H, RES_1_L, RES_1_dHL, 
-RES_2_B, RES_2_C, RES_2_D, RES_2_E, RES_2_H, RES_2_L, RES_2_dHL, RES_3_B, RES_3_C, RES_3_D, RES_3_E, RES_3_H, RES_3_L, RES_3_dHL, 
-RES_4_B, RES_4_C, RES_4_D, RES_4_E, RES_4_H, RES_4_L, RES_4_dHL, RES_5_B, RES_5_C, RES_5_D, RES_5_E, RES_5_H, RES_5_L, RES_5_dHL, 
-RES_6_B, RES_6_C, RES_6_D, RES_6_E, RES_6_H, RES_6_L, RES_6_dHL, RES_7_B, RES_7_C, RES_7_D, RES_7_E, RES_7_H, RES_7_L, RES_7_dHL, 
-SET_0_B, SET_0_C, SET_0_D, SET_0_E, SET_0_H, SET_0_L, SET_0_dHL, SET_1_B, SET_1_C, SET_1_D, SET_1_E, SET_1_H, SET_1_L, SET_1_dHL, 
-SET_2_B, SET_2_C, SET_2_D, SET_2_E, SET_2_H, SET_2_L, SET_2_dHL, SET_3_B, SET_3_C, SET_3_D, SET_3_E, SET_3_H, SET_3_L, SET_3_dHL, 
-SET_4_B, SET_4_C, SET_4_D, SET_4_E, SET_4_H, SET_4_L, SET_4_dHL, SET_5_B, SET_5_C, SET_5_D, SET_5_E, SET_5_H, SET_5_L, SET_5_dHL, 
-SET_6_B, SET_6_C, SET_6_D, SET_6_E, SET_6_H, SET_6_L, SET_6_dHL, SET_7_B, SET_7_C, SET_7_D, SET_7_E, SET_7_H, SET_7_L, SET_7_dHL, 
-};
+void decode(uint8_t op);
+uint8_t running=1;
+uint8_t *rom;
+uint8_t tByte;
+uint16_t tWord;
 
 uint8_t CPU[12];
 #define B CPU[1]
@@ -56,14 +24,99 @@ uint8_t CPU[12];
 #define AF ((uint16_t*)CPU)[3]
 #define PC ((uint16_t*)CPU)[4]
 #define SP ((uint16_t*)CPU)[5]
-#define Zflag 7
-#define Nflag 6
-#define Hflag 5
-#define Carry 4
+#define Zflag 0x80
+#define Nflag 0x40
+#define Hflag 0x20
+#define Cflag 0x10
 
-void decode(uint8_t op);
-uint8_t running=1;
-uint8_t *rom;
+#define ADD8(R)\
+    tWord=A+R;\
+    F=(tWord&0xFF?0:Zflag)|\
+        (((A^R^tWord)&0x10)?Hflag:0)|\
+        (tWord&0xFF00?Cflag:0);\
+    A=tWord&0xFF;
+
+#define ADC8(R)\
+    tWord=A+R+F&Cflag;\
+    F=(tWord&0xFF?0:Zflag)|\
+        (((A^R^tWord)&0x10)?Hflag:0)|\
+        (tWord&0xFF00?Cflag:0);\
+    A=tWord&0xFF;
+
+#define SUB(R)\
+    tWord=A-R;\
+    F=(tWord&0xFF?0:Zflag)|\
+        Nflag|\
+        (((A^R^tWord)&0x10)?Hflag:0)|\
+        (tWord&0xFF00?Cflag:0);\
+    A=tWord&0xFF;
+
+#define SBC(R)\
+    tWord=A-R-F&Cflag;\
+    F=(tWord&0xFF?0:Zflag)|\
+        Nflag|\
+        (((A^R^tWord)&0x10)?Hflag:0)|\
+        (tWord&0xFF00?Cflag:0);\
+    A=tWord&0xFF;
+
+#define AND(R)\
+    A&=R;\
+    F=(A?0:Zflag)|\
+        Hflag;
+
+#define XOR(R)\
+    A^=R;\
+    F=(A?0:Zflag);
+
+#define OR(R)\
+    A|=R;\
+    F=(A?0:Zflag);
+
+#define CP(R)\
+    tWord=A-R;\
+    F=(tWord&0xFF?0:Zflag)|\
+        Nflag|\
+        (((A^R^tWord)&0x10)?Hflag:0)|\
+        (tWord&0xFF00?Cflag:0);
+
+enum opcodes{
+NOP,     LD_BC_w, LD_dBC_A,  INC_BC, INC_B,   DEC_B,   LD_B_b,   RLCA, LD_dw_SP, ADD_HL_BC, LD_A_dBC,  DEC_BC, INC_C, DEC_C, LD_C_b, RRCA,
+STOP,    LD_DE_w, LD_dDE_A,  INC_DE, INC_D,   DEC_D,   LD_D_b,   RLA,  JR_b,     ADD_HL_DE, LD_A_dDE,  DEC_DE, INC_E, DEC_E, LD_E_b, RRA,
+JR_NZ_b, LD_HL_w, LD_dHLp_A, INC_HL, INC_H,   DEC_H,   LD_H_b,   DAA,  JR_Z_b,   ADD_HL_HL, LD_A_dHLp, DEC_HL, INC_L, DEC_L, LD_L_b, CPL,
+JR_NC_b, LD_SP_w, LD_dHLm_A, INC_SP, INC_dHL, DEC_dHL, LD_dHL_b, SCF,  JR_C_b,   ADD_HL_SP, LD_A_dHLm, DEC_SP, INC_A, DEC_A, LD_A_b, CCF,
+LD_B_B,   LD_B_C,   LD_B_D,   LD_B_E,   LD_B_H,   LD_B_L,   LD_B_dHL, LD_B_A,   LD_C_B, LD_C_C, LD_C_D, LD_C_E, LD_C_H, LD_C_L, LD_C_dHL, LD_C_A,
+LD_D_B,   LD_D_C,   LD_D_D,   LD_D_E,   LD_D_H,   LD_D_L,   LD_D_dHL, LD_D_A,   LD_E_B, LD_E_C, LD_E_D, LD_E_E, LD_E_H, LD_E_L, LD_E_dHL, LD_E_A,
+LD_H_B,   LD_H_C,   LD_H_D,   LD_H_E,   LD_H_H,   LD_H_L,   LD_H_dHL, LD_H_A,   LD_L_B, LD_L_C, LD_L_D, LD_L_E, LD_L_H, LD_L_L, LD_L_dHL, LD_L_A,
+LD_dHL_B, LD_dHL_C, LD_dHL_D, LD_dHL_E, LD_dHL_H, LD_dHL_L, HALT,     LD_dHL_A, LD_A_B, LD_A_C, LD_A_D, LD_A_E, LD_A_H, LD_A_L, LD_A_dHL, LD_A_A,
+ADD_A_B, ADD_A_C, ADD_A_D, ADD_A_E, ADD_A_H, ADD_A_L, ADD_A_dHL, ADD_A_A, ADC_A_B, ADC_A_C, ADC_A_D, ADC_A_E, ADC_A_H, ADC_A_L, ADC_A_dHL, ADC_A_A,
+SUB_B,   SUB_C,   SUB_D,   SUB_E,   SUB_H,   SUB_L,   SUB_dHL,   SUB_A,   SBC_A_B, SBC_A_C, SBC_A_D, SBC_A_E, SBC_A_H, SBC_A_L, SBC_A_dHL, SBC_A_A,
+AND_B,   AND_C,   AND_D,   AND_E,   AND_H,   AND_L,   AND_dHL,   AND_A,   XOR_B,   XOR_C,   XOR_D,   XOR_E,   XOR_H,   XOR_L,   XOR_dHL,   XOR_A,
+OR_B,    OR_C,    OR_D,    OR_E,    OR_H,    OR_L,    OR_dHL,    OR_A,    CP_B,    CP_C,    CP_D,    CP_E,    CP_H,    CP_L,    CP_dHL,    CP_A,
+RET_NZ,   POP_BC, JP_NZ_w, JP_w,  CALL_NZ_w, PUSH_BC, ADD_A_b, RST_00H, RET_Z,      RET,      JP_Z_w,  CB,    CALL_Z_w, CALL_w, ADC_A_b, RST_08H,
+RET_NC,   POP_DE, JP_NC_w, fail1, CALL_NC_w, PUSH_DE, SUB_b,   RST_10H, RET_C,      RETI,     JP_C_w,  fail2, CALL_C_w, fail3,  SBC_A_b, RST_18H,
+LDH_db_A, POP_HL, LD_dC_A, fail4, fail5,     PUSH_HL, AND_b,   RST_20H, ADD_SP_b,   JP_dHL,   LD_dw_A, fail6, fail7,    fail8,  XOR_b,   RST_28H,
+LDH_A_db, POP_AF, LD_A_dC, DI,    fail9,     PUSH_AF, OR_b,    RST_30H, LD_HL_SP_b, LD_SP_HL, LD_A_dw, EI,    failA,    failB,  CP_b,    RST_38H
+};
+
+enum opcodesCB{
+RLC_B, RLC_C, RLC_D, RLC_E, RLC_H, RLC_L, RLC_dHL, RLC_A, RRC_B, RRC_C, RRC_D, RRC_E, RRC_H, RRC_L, RRC_dHL, RRC_A,
+RL_B, RL_C, RL_D, RL_E, RL_H, RL_L, RL_dHL, RL_A, RR_B, RR_C, RR_D, RR_E, RR_H, RR_L, RR_dHL, RR_A,
+SLA_B, SLA_C, SLA_D, SLA_E, SLA_H, SLA_L, SLA_dHL, SLA_A, SRA_B, SRA_C, SRA_D, SRA_E, SRA_H, SRA_L, SRA_dHL, SRA_A,
+SWAP_B, SWAP_C, SWAP_D, SWAP_E, SWAP_H, SWAP_L, SWAP_dHL, SWAP_A, SRL_B, SRL_C, SRL_D, SRL_E, SRL_H, SRL_L, SRL_dHL, SRL_A,
+BIT_0_B, BIT_0_C, BIT_0_D, BIT_0_E, BIT_0_H, BIT_0_L, BIT_0_dHL, BIT_1_B, BIT_1_C, BIT_1_D, BIT_1_E, BIT_1_H, BIT_1_L, BIT_1_dHL,
+BIT_2_B, BIT_2_C, BIT_2_D, BIT_2_E, BIT_2_H, BIT_2_L, BIT_2_dHL, BIT_3_B, BIT_3_C, BIT_3_D, BIT_3_E, BIT_3_H, BIT_3_L, BIT_3_dHL,
+BIT_4_B, BIT_4_C, BIT_4_D, BIT_4_E, BIT_4_H, BIT_4_L, BIT_4_dHL, BIT_5_B, BIT_5_C, BIT_5_D, BIT_5_E, BIT_5_H, BIT_5_L, BIT_5_dHL,
+BIT_6_B, BIT_6_C, BIT_6_D, BIT_6_E, BIT_6_H, BIT_6_L, BIT_6_dHL, BIT_7_B, BIT_7_C, BIT_7_D, BIT_7_E, BIT_7_H, BIT_7_L, BIT_7_dHL,
+RES_0_B, RES_0_C, RES_0_D, RES_0_E, RES_0_H, RES_0_L, RES_0_dHL, RES_1_B, RES_1_C, RES_1_D, RES_1_E, RES_1_H, RES_1_L, RES_1_dHL,
+RES_2_B, RES_2_C, RES_2_D, RES_2_E, RES_2_H, RES_2_L, RES_2_dHL, RES_3_B, RES_3_C, RES_3_D, RES_3_E, RES_3_H, RES_3_L, RES_3_dHL,
+RES_4_B, RES_4_C, RES_4_D, RES_4_E, RES_4_H, RES_4_L, RES_4_dHL, RES_5_B, RES_5_C, RES_5_D, RES_5_E, RES_5_H, RES_5_L, RES_5_dHL,
+RES_6_B, RES_6_C, RES_6_D, RES_6_E, RES_6_H, RES_6_L, RES_6_dHL, RES_7_B, RES_7_C, RES_7_D, RES_7_E, RES_7_H, RES_7_L, RES_7_dHL,
+SET_0_B, SET_0_C, SET_0_D, SET_0_E, SET_0_H, SET_0_L, SET_0_dHL, SET_1_B, SET_1_C, SET_1_D, SET_1_E, SET_1_H, SET_1_L, SET_1_dHL,
+SET_2_B, SET_2_C, SET_2_D, SET_2_E, SET_2_H, SET_2_L, SET_2_dHL, SET_3_B, SET_3_C, SET_3_D, SET_3_E, SET_3_H, SET_3_L, SET_3_dHL,
+SET_4_B, SET_4_C, SET_4_D, SET_4_E, SET_4_H, SET_4_L, SET_4_dHL, SET_5_B, SET_5_C, SET_5_D, SET_5_E, SET_5_H, SET_5_L, SET_5_dHL,
+SET_6_B, SET_6_C, SET_6_D, SET_6_E, SET_6_H, SET_6_L, SET_6_dHL, SET_7_B, SET_7_C, SET_7_D, SET_7_E, SET_7_H, SET_7_L, SET_7_dHL,
+};
+
 
 int main(int argc, char *argv[]){
     if(argc!=2){
--- opcodes.h
+++ opcodes.h
@@ -1,49 +1,77 @@
 case NOP:
     break;
 case STOP:
+    printf("STOP");
+    running=0;
     break;
 case HALT:
+    printf("HALT");
+    running=0;
     break;
 
-case ADD_A_A: A+=A; break;
-case ADD_A_B: A+=B; break;
-case ADD_A_C: A+=C; break;
-case ADD_A_D: A+=D; break;
-case ADD_A_E: A+=E; break;
-case ADD_A_H: A+=H; break;
-case ADD_A_L: A+=L; break;
+case ADD_A_A: ADD8(A); break;
+case ADD_A_B: ADD8(B); break;
+case ADD_A_C: ADD8(C); break;
+case ADD_A_D: ADD8(D); break;
+case ADD_A_E: ADD8(E); break;
+case ADD_A_H: ADD8(H); break;
+case ADD_A_L: ADD8(L); break;
 
-case SUB_A: A-=A; break;
-case SUB_B: A-=B; break;
-case SUB_C: A-=C; break;
-case SUB_D: A-=D; break;
-case SUB_E: A-=E; break;
-case SUB_H: A-=H; break;
-case SUB_L: A-=L; break;
+case ADC_A_A: ADC8(A); break;
+case ADC_A_B: ADC8(B); break;
+case ADC_A_C: ADC8(C); break;
+case ADC_A_D: ADC8(D); break;
+case ADC_A_E: ADC8(E); break;
+case ADC_A_H: ADC8(H); break;
+case ADC_A_L: ADC8(L); break;
 
-case AND_A: A&=A; break;
-case AND_B: A&=B; break;
-case AND_C: A&=C; break;
-case AND_D: A&=D; break;
-case AND_E: A&=E; break;
-case AND_H: A&=H; break;
-case AND_L: A&=L; break;
+case SUB_A: SUB(A); break;
+case SUB_B: SUB(B); break;
+case SUB_C: SUB(C); break;
+case SUB_D: SUB(D); break;
+case SUB_E: SUB(E); break;
+case SUB_H: SUB(H); break;
+case SUB_L: SUB(L); break;
 
-case OR_A: A|=A; break;
-case OR_B: A|=B; break;
-case OR_C: A|=C; break;
-case OR_D: A|=D; break;
-case OR_E: A|=E; break;
-case OR_H: A|=H; break;
-case OR_L: A|=L; break;
+case SBC_A_A: SBC(A); break;
+case SBC_A_B: SBC(B); break;
+case SBC_A_C: SBC(C); break;
+case SBC_A_D: SBC(D); break;
+case SBC_A_E: SBC(E); break;
+case SBC_A_H: SBC(H); break;
+case SBC_A_L: SBC(L); break;
 
-case XOR_A: A^=A; break;
-case XOR_B: A^=B; break;
-case XOR_C: A^=C; break;
-case XOR_D: A^=D; break;
-case XOR_E: A^=E; break;
-case XOR_H: A^=H; break;
-case XOR_L: A^=L; break;
+case AND_A: AND(A); break;
+case AND_B: AND(B); break;
+case AND_C: AND(C); break;
+case AND_D: AND(D); break;
+case AND_E: AND(E); break;
+case AND_H: AND(H); break;
+case AND_L: AND(L); break;
+
+case XOR_A: XOR(A); break;
+case XOR_B: XOR(B); break;
+case XOR_C: XOR(C); break;
+case XOR_D: XOR(D); break;
+case XOR_E: XOR(E); break;
+case XOR_H: XOR(H); break;
+case XOR_L: XOR(L); break;
+
+case OR_A: OR(A); break;
+case OR_B: OR(B); break;
+case OR_C: OR(C); break;
+case OR_D: OR(D); break;
+case OR_E: OR(E); break;
+case OR_H: OR(H); break;
+case OR_L: OR(L); break;
+
+case CP_A: CP(A); break;
+case CP_B: CP(B); break;
+case CP_C: CP(C); break;
+case CP_D: CP(D); break;
+case CP_E: CP(E); break;
+case CP_H: CP(H); break;
+case CP_L: CP(L); break;
 
 case JP_w: PC=rom[PC]|rom[PC+1]<<8; break;
 
